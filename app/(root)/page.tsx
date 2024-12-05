ed7595deb6e8855bfecfe0941b6c975b4030cb73
blue4e2 <blue.4e2@gmail.com>
2024-12-05 06:37:50 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
